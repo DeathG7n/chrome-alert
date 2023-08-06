37c5a25b736bd299b378f971a313c872c0b8b1b6
DeathG7n <christariccykid55@gmail.com>
2023-08-06 12:54:01 +0100

    changes

--- src/app/api/alert/route.js
+++ src/app/api/alert/route.js
@@ -5,9 +5,7 @@ const nodemailer = require('nodemailer');
 const connection = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=36807');
 import { ema } from "ta.js";
 
-let interval = setInterval(()=> getTicksHistory(), 1000) 
-let trade = false
-let emails = 5
+let trade = new Date()
 
 // Create a transporter object using SMTP
 const transporter = nodemailer.createTransport({
@@ -33,51 +31,39 @@ const ticks_history_request = {
     count: 21,
     end: 'latest',
     style: 'candles',
-    granularity: 60,
+    granularity: 300,
 };
 
-const getTicksHistory = async () => {
-    const candles = await api.ticksHistory(ticks_history_request);
-    const closePrices = candles?.candles?.map(i => {return i?.close - 35.495})
-    const highPrices = candles?.candles?.map(i => {return i?.high - 35.4976})
-    const lowPrices = candles?.candles?.map(i => {return i?.low - 35.5046})
-    const openPrices = candles?.candles?.map(i => {return i?.open - 35.5046})
-    const sma = ema(closePrices, closePrices.length)
-    if(closePrices[19] > sma && openPrices[19] < sma ||  closePrices[19] < sma && openPrices[19] > sma){
-        // Define the email content
-        const mailOptions = {
-            from: "christariccykid55@gmail.com",
-            to: "meliodasdemonk8ng@gmail.com",
-            subject: `Market Structure Broken at ${sma}`,
-            text: 'Potential Buy Signal'
-        };
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error('Error sending email:', error);
-            } else {
-                console.log('Email sent successfully:', info.response);
-            }
-        });
-        pause()
-    }
-    console.log(sma)
-};
+
 
 
 export async function GET(req){
-    const mailOptions = {
-        from: "christariccykid55@gmail.com",
-        to: "meliodasdemonk8ng@gmail.com",
-        subject: `Market Structure Broken at `,
-        text: 'Potential Buy Signal'
-    };
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error('Error sending email:', error);
-        } else {
-            console.log('Email sent successfully:', info.response);
+    const getTicksHistory = async () => {
+        const candles = await api.ticksHistory(ticks_history_request);
+        const closePrices = candles?.candles?.map(i => {return i?.close - 35.495})
+        const highPrices = candles?.candles?.map(i => {return i?.high - 35.4976})
+        const lowPrices = candles?.candles?.map(i => {return i?.low - 35.5046})
+        const openPrices = candles?.candles?.map(i => {return i?.open - 35.5046})
+        const sma = ema(closePrices, closePrices.length)
+        if(closePrices[19] > sma && openPrices[19] < sma ||  closePrices[19] < sma && openPrices[19] > sma){
+            // Define the email content
+            const mailOptions = {
+                from: "christariccykid55@gmail.com",
+                to: "meliodasdemonk8ng@gmail.com",
+                subject: `Market Structure Broken at ${sma}`,
+                text: 'Potential Buy Signal'
+            };
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.error('Error sending email:', error);
+                } else {
+                    console.log('Email sent successfully:', info.response);
+                }
+            });
         }
-    });
+        console.log(sma)
+    };
+    getTicksHistory()
     return NextResponse.json(trade)
 }
 
